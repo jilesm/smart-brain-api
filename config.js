c51c7504316077611deaa157851a486ab287ba70
Marcus Jiles <jilesm@mail.gvsu.edu>
2019-12-16 21:40:08 -0500

    Add working files

---
+++ config.js
@@ -0,0 +1,6 @@
+
+    const host = '127.0.0.1';
+    const user = 'postgres';
+    const pass = 'markkiler23';
+    const database = 'smartbrain';
+    
---
+++ controllers/image.js
@@ -0,0 +1,31 @@
+const Clarifai = require('clarifai');
+
+//You must add your own API key here from Clarifai.
+const app = new Clarifai.App({
+ apiKey: '304c5439f1334eb5a97b6c39f7a44a93'
+});
+
+const handleApiCall = (req, res) => {
+  app.models
+    .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    .then(data => {
+      res.json(data);
+    })
+    .catch(err => res.status(400).json('unable to work with API'))
+}
+
+const handleImage = (req, res, db) => {
+  const { id } = req.body;
+  db('users').where('id', '=', id)
+  .increment('entries', 1)
+  .returning('entries')
+  .then(entries => {
+    res.json(entries[0]);
+  })
+  .catch(err => res.status(400).json('unable to get entries'))
+}
+
+module.exports = {
+  handleImage,
+  handleApiCall
+}
